8c33029e73246fc250155d392609dc520d688fe5
Saifullakh1 <saifullakh7@gmail.com>
2024-02-06 11:50:07 +0600

    first commit

---
+++ apps/clothes/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from apps.clothes.models import Clothes
+
+admin.site.register(Clothes)
---
+++ apps/clothes/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Clothes(models.Model):
+    class Currency(models.TextChoices):
+        som = "som"
+        dollar = "dollar"
+        euro = "euro"
+
+    class Size(models.TextChoices):
+        m = "M"
+        l = "L"
+        xl = "XL"
+    title = models.CharField(
+        max_length=250, verbose_name="Название"
+    )
+    description = models.TextField(
+        verbose_name="Описание"
+    )
+    image = models.ImageField(
+        upload_to="clothes_images", verbose_name="Картинка"
+    )
+    size = models.CharField(
+        max_length=150, choices=Size.choices,
+        default=Size.l, verbose_name="Размер"
+    )
+    price = models.IntegerField(
+        default=0, verbose_name="Цена"
+    )
+    currency = models.CharField(
+        max_length=150, choices=Currency.choices,
+        default=Currency.som, verbose_name="Валюта"
+    )
+    is_active = models.BooleanField(
+        default=True, verbose_name="Активный"
+    )
+
+    class Meta:
+        verbose_name = "Одежда"
+        verbose_name_plural = "Одежды"
+
+    def __str__(self):
+        return self.title
